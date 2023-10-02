9b16917be2252d5696d04f0066456e445c661b8f
DiyanKomalaSari <144796240+DiyanKomalaSari@users.noreply.github.com>
2023-10-02 08:40:36 +0700

    Create halaman_siswa.php

---
+++ halaman_siswa.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Halaman siswa</title>
+</head>
+<body>
+<?php 
+session_start();
+//cek apakah yg mengakses halaman ini sudah login
+if($_SESSION['level']==" "){
+header("location:index.php?pesan=gagal");
+}
+ ?>
+<h1>Halaman pegawai</h1>
+
+<p>Hallo<b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['level']; ?> </b></p>
+<a href="logout.php">LOGOUT</a>
+
+<br/>
+<br/>
+
+<a> Membuat login multi user dengan php</a>
+</body>
+</html>
